93f32f4f59fe23668a74dd867dc35f303c4aa035
Hoon <yanghoon5@gmail.com>
2026-04-28 22:10:50 +0900

    Add Harbor service manifest and update image versions in scripts and configuration

--- packages/rke2-harbor/scripts/harbor/images.sh
+++ packages/rke2-harbor/scripts/harbor/images.sh
@@ -1,37 +1,40 @@
+#!/bin/bash
 
 # zarf dev find-images
 IMAGES=(
-  "goharbor/chartmuseum-photon:v1.10.2"
-  "goharbor/clair-adapter-photon:v1.10.2"
-  "goharbor/clair-photon:v1.10.2"
-  "goharbor/harbor-core:v1.10.2"
-  "goharbor/harbor-db:v1.10.2"
-  "goharbor/harbor-jobservice:v1.10.2"
-  "goharbor/harbor-portal:v1.10.2"
-  "goharbor/harbor-registryctl:v1.10.2"
-  "goharbor/notary-server-photon:v1.10.2"
-  "goharbor/notary-signer-photon:v1.10.2"
-  "goharbor/redis-photon:v1.10.2"
-  "goharbor/registry-photon:v1.10.2"
+  goharbor/harbor-core:v2.13.5
+  goharbor/harbor-jobservice:v2.13.5
+  goharbor/harbor-portal:v2.13.5
+  goharbor/harbor-registryctl:v2.13.5
+  goharbor/redis-photon:v2.13.5
+  goharbor/registry-photon:v2.13.5
 )
 
+TMP_DIR=.tmp
+
+## init
+mkdir -p "$TMP_DIR"
+
+## functions
 tar_file_name() {
   echo "$1.tar" | tr ":/" "-"
 }
 
 download() {
   while IFS= read -r image; do
-    tar_file="$(tar_file_name $image)"
-    echo "Pulling $image and saving as $tar_file"
+    tar_file="$TMP_DIR/$(tar_file_name $image)"
+    echo "Pulling $image -> $tar_file"
 
     zarf tools registry pull --format=tarball \
       "$image" "$tar_file"
+    # zarf tools registry pull "$image"
+    # zarf tools registry pull "$image" --format=tarball > "$tar_file"
   done <<< "$(printf '%s\n' "${IMAGES[@]}")"
 }
 
 clean() {
   while IFS= read -r image; do
-    tar_file="$(tar_file_name $image)"
+    tar_file="$TMP_DIR/$(tar_file_name $image)"
 
     if [ -f "$tar_file" ]; then
       echo "Removing $tar_file"
@@ -43,6 +46,15 @@ clean() {
   done <<< "$(printf '%s\n' "${IMAGES[@]}")"
 }
 
+files() {
+  while IFS= read -r image; do
+    tar_file="$TMP_DIR/$(tar_file_name $image)"
+    echo "- source: $tar_file"
+    echo "  target: /var/lib/rancher/rke2/agent/images/$tar_file"
+  done <<< "$(printf '%s\n' "${IMAGES[@]}")"
+}
+
+## main
 case "$1" in
   "download")
     download
@@ -50,8 +62,11 @@ case "$1" in
   "clean")
     clean
     ;;
+  "files")
+    files
+    ;;
   *)
-    echo "Usage: $0 {download|clean}"
+    echo "Usage: $0 {download|clean|files}"
     exit 1
     ;;
 esac
