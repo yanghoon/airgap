ee85e11173610975b9fe4b3c842e62b6225f43b2
Hoon <yanghoon5@gmail.com>
2026-05-14 20:31:24 +0900

    Dify: add initial configuration files for embedding and deployment

---
+++ packages/dify/dify-embedding/config.js
@@ -0,0 +1,18 @@
+ window.difyChatbotConfig = {
+  token: 'bBTPwiwLcNsTY8F9',
+  baseUrl: 'http://localhost',
+  inputs: {
+    // You can define the inputs from the Start node here
+    // key is the variable name
+    // e.g.
+    // name: "NAME"
+  },
+  systemVariables: {
+    // user_id: 'YOU CAN DEFINE USER ID HERE',
+    // conversation_id: 'YOU CAN DEFINE CONVERSATION ID HERE, IT MUST BE A VALID UUID',
+  },
+  userVariables: {
+    // avatar_url: 'YOU CAN DEFINE USER AVATAR URL HERE',
+    // name: 'YOU CAN DEFINE USER NAME HERE',
+  },
+ }
